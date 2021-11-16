5865599d12dfb07e7d4daf7f7a148ccf3140fd12
Fiso-Maker <youn110158@naver.com>
2021-11-16 16:43:49 +0900

    0.1

---
+++ Game_Project_2021_2/Assets/Scripts/Timer.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Timer : MonoBehaviour
+{
+    public float LimitTime;
+    public Text text_Timer;
+
+    int minute;
+    float second;
+    // Start is called before the first frame update
+    void Start()
+    {
+        minute = (int)LimitTime/60;
+        second = LimitTime%60;
+        text_Timer.text = "";
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        LimitTime -= Time.deltaTime;
+        minute = (int)LimitTime/60;
+        second = LimitTime%60;
+
+        text_Timer.text = minute + ": " +Mathf.Round(second);
+    }
+}
